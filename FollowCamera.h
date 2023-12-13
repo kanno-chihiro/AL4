653f8e7dd692100a13277466690b8073c7d4a52a
菅野　地紘 <k022g0111@g.neec.ac.jp>
2023-12-13 13:16:09 +0900

    プロジェクト ファイルを追加します。

---
+++ FollowCamera.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "WorldTransform.h"
+#include "ViewProjection.h"
+#include "MatrixTrans.h"
+#include "Input.h"
+
+class FollowCamera
+{
+public:
+
+	void Initialize();
+
+	void Update();
+
+	//void Draw();
+
+	void SetTarget(const WorldTransform* target) { target_ = target; }
+
+	const ViewProjection& GetViewProjection() { return viewProjection_; }
+
+private:
+
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	const WorldTransform* target_ = nullptr;
+
+};
